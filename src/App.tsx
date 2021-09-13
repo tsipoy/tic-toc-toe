f82baab1cde814e9be35fc8b4d226679833b3066
student <natacha.har@onja.org>
2021-09-13 11:11:37 +0300

    Adds react router for the gameScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import './App.css';
 import StartScreen from './components/StartScreen';
+import GameScreen from './components/GameStart';
+import { 
+  Switch,
+  Route,
+} from "react-router-dom";
+import { Paths } from "./helpers/routerHelper";
 
 function App() {
   return (
-      <StartScreen 
-        type= '' 
-        placeholder= ''
-        label= ''
-        onChange= {() => null}
-      />
+    <div className="App">
+      <Switch>
+          <Route path={Paths.GAME_SCREEN}>
+              <GameScreen />
+          </Route>
+          <Route path={Paths.START_SCREEN}>
+            <StartScreen 
+              type= '' 
+              placeholder= ''
+              label= ''
+              onChange= {() => null}
+            /> 
+          </Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/GameStart.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const GameScreen = () => {
+    return (
+        <div>Game screen</div>
+    )
+}
+
+export default GameScreen
